c3e5d6d63502e3907d27a978f7d96428d4eaa7d7
1Mr-Newton <eaidoo015@gmail.com>
2024-05-27 12:06:34 +0000

    Add asyncio functionality to main.py

--- main.py
+++ main.py
@@ -1,13 +1,20 @@
 from fastapi import FastAPI
 import uvicorn
+import asyncio
 
 app = FastAPI()
 
 
+async def do_something():
+    await asyncio.sleep(1.3)  # Non-blocking sleep
+    return "Done"
+
+
 @app.get("/")
-def read_root():
+async def read_root():
+    await do_something()
     return {"Hello": "World"}
 
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8001)
